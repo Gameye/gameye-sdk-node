ff9e65019bc1dbf4fdf04e30563e4d97491d03a6
Elmer Bulthuis <elmerbulthuis@gmail.com>
2018-08-14 16:33:05 +0200

    empty query / command

--- src/clients/gameye.ts
+++ src/clients/gameye.ts
@@ -1,5 +1,9 @@
 import * as errors from "../errors";
 import { isEmpty } from "../utils";
+import { queryGame } from "./gameye-game";
+import { commandStartMatch, commandStopMatch, queryMatch } from "./gameye-match";
+import { queryStatistic } from "./gameye-statistic";
+import { queryTemplate } from "./gameye-template";
 
 export interface GameyeClientConfig {
     endpoint: string;
@@ -12,9 +16,26 @@ export class GameyeClient {
         endpoint: process.env.GAMEYE_API_ENDPOINT || "",
         token: process.env.GAMEYE_API_TOKEN || "",
     });
+
+    // #region Commands
+
+    public commandStartMatch = commandStartMatch;
+    public commandStopMatch = commandStopMatch;
+
+    // #endregion
+
+    // #region Queries
+
+    public queryStatistic = queryStatistic;
+    public queryTemplate = queryTemplate;
+    public queryGame = queryGame;
+    public queryMatch = queryMatch;
+
+    // #endregion
+
     private config: Readonly<GameyeClientConfig>;
 
-    public constructor(config: Partial<GameyeClient>) {
+    public constructor(config: Partial<GameyeClientConfig>) {
         this.config = Object.freeze({
             ...this.constructor.prototype.defaultConfig,
             ...config,

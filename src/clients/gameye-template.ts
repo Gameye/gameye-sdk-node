27d49be880d2cc36d48dff82131b1f29f0da918c
Elmer Bulthuis <elmerbulthuis@gmail.com>
2018-08-14 16:42:12 +0200

    cleanupo

--- src/clients/gameye-template.ts
+++ src/clients/gameye-template.ts
@@ -3,12 +3,6 @@ import { GameyeClient } from "./gameye";
 
 export function queryTemplate(
     this: GameyeClient,
-) {
-    throw new errors.NotImplemented();
-}
-
-export function subscribeTemplate(
-    this: GameyeClient,
     subscribe = false,
 ) {
     throw new errors.NotImplemented();

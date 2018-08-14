95f3e95c3121fbd886edab8e37b750a8e82a1ea0
Elmer Bulthuis <elmerbulthuis@gmail.com>
2018-08-14 16:39:48 +0200

    spec

---
+++ src/clients/gameye-match.spec.ts
@@ -0,0 +1,57 @@
+import * as test from "blue-tape";
+import * as errors from "../errors";
+import { GameyeClient } from "./gameye";
+
+test("GameyeClient commandStartMatch", async t => {
+    const client = new GameyeClient({
+        token: "123",
+        endpoint: "http://localhost",
+    });
+    try {
+        client.commandStartMatch({});
+        t.fail();
+    }
+    catch (err) {
+        if (err instanceof errors.NotImplemented) {
+            t.pass();
+            err = null;
+        }
+        if (err) throw err;
+    }
+});
+
+test("GameyeClient commandStopMatch", async t => {
+    const client = new GameyeClient({
+        token: "123",
+        endpoint: "http://localhost",
+    });
+    try {
+        client.commandStopMatch({});
+        t.fail();
+    }
+    catch (err) {
+        if (err instanceof errors.NotImplemented) {
+            t.pass();
+            err = null;
+        }
+        if (err) throw err;
+    }
+});
+
+test("GameyeClient queryMatch", async t => {
+    const client = new GameyeClient({
+        token: "123",
+        endpoint: "http://localhost",
+    });
+    try {
+        client.queryMatch();
+        t.fail();
+    }
+    catch (err) {
+        if (err instanceof errors.NotImplemented) {
+            t.pass();
+            err = null;
+        }
+        if (err) throw err;
+    }
+});
